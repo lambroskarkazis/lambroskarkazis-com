bbf23048010ac07cac7010b674631124f1657273
Lambros Karkazis <lkarkazis@gmail.com>
2026-05-02 15:08:52 -0500

    Libraries Section
    Add Initial Library Scaffolding.
    
    Add a post for my Chinatown visit.

--- src/content.config.ts
+++ src/content.config.ts
@@ -16,4 +16,21 @@ const blog = defineCollection({
 		}),
 });
 
-export const collections = { blog };
+const libraries = defineCollection({
+	// Load Markdown and MDX files in the `src/content/libraries/` directory.
+	loader: glob({ base: './src/content/libraries', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			// Transform string to Date object
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			heroImage: image().optional(),
+			cplId: z.number(),
+		}),
+});
+
+
+export const collections = { blog, libraries };
---
+++ src/content/libraries/chinatown.md
@@ -0,0 +1,12 @@
+---
+title: 'Chinatown'
+description: 'Great view right off of the Red Line'
+pubDate: 'April 13 2026'
+cplId: 20
+---
+
+I really enjoyed the Chinatown public library. It is right off of the Red Line. There are alot of great restraurants and shops to walk around to take a break. I came down and ate lunch, worked for a while, then Christine joined me for dinner.
+
+They had a huge Chinese language selection.
+
+The library was not as big inside as it looked.
